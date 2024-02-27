5bfa436159b92704eb82b3aaaa750d9c64a73f19
Han-Wen Chang <ondlraehsconegrb@gmail.com>
2024-02-27 16:17:58 +0800

    加入專案檔案。

---
+++ feb27_2024_csharp_review/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace feb27_2024_csharp_review
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBoxInTabCreateObjects = new System.Windows.Forms.ListBox();
+            this.buttonReview0 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(711, 375);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBoxInTabCreateObjects);
+            this.tabPage1.Controls.Add(this.buttonReview0);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Size = new System.Drawing.Size(703, 346);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "產生物件";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listBoxInTabCreateObjects
+            // 
+            this.listBoxInTabCreateObjects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxInTabCreateObjects.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxInTabCreateObjects.FormattingEnabled = true;
+            this.listBoxInTabCreateObjects.ItemHeight = 19;
+            this.listBoxInTabCreateObjects.Location = new System.Drawing.Point(3, 2);
+            this.listBoxInTabCreateObjects.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBoxInTabCreateObjects.Name = "listBoxInTabCreateObjects";
+            this.listBoxInTabCreateObjects.Size = new System.Drawing.Size(697, 300);
+            this.listBoxInTabCreateObjects.TabIndex = 1;
+            // 
+            // buttonReview0
+            // 
+            this.buttonReview0.BackColor = System.Drawing.Color.Yellow;
+            this.buttonReview0.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonReview0.Location = new System.Drawing.Point(3, 302);
+            this.buttonReview0.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonReview0.Name = "buttonReview0";
+            this.buttonReview0.Size = new System.Drawing.Size(697, 42);
+            this.buttonReview0.TabIndex = 0;
+            this.buttonReview0.Text = "複習#0";
+            this.buttonReview0.UseVisualStyleBackColor = false;
+            this.buttonReview0.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 375);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBoxInTabCreateObjects;
+        private System.Windows.Forms.Button buttonReview0;
+    }
+}
+
---
+++ feb27_2024_csharp_review/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace feb27_2024_csharp_review
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region   #-1
+            // todo #-1  執行此 App 暗黃色按鈕 會去執行的程式碼 已寫好如下
+            #endregion
+            MessageBox.Show("msit c sharp 複習 #0 " );
+            System.Data.SqlClient.SqlConnection conn = null;
+            try
+            {
+                #region  todo #0
+                // todo #0 產生 System.Data.SqlClient 命名空間下的 SqlConnection 類別的物件
+                //  並將此 產生的物件的參考存到上行宣告的 conn 變數
+                #endregion
+
+
+
+                conn.ConnectionString = "server=.;initial catalog=northwind;integrated security=true";
+                #region    #1
+                // todo #1 以上面產生的 SqlConnection 類別的物件
+                //    來呼叫執行個體方法 Open 此方法沒設計引數(或稱呼為參數)
+                #endregion
+                
+                System.Data.SqlClient.SqlCommand sqlCmd = null;
+                #region  #2
+                // todo #2 以上面產生的 SqlConnection 類別的物件
+                //    來呼叫執行個體方法 CreateCommand 此方法沒設計引數(或稱呼為參數)
+                //    並將呼叫 此方法的傳回值 存到上行宣告的 sqlCmd 變數
+                #endregion
+                conn.Open();
+
+                #region   #5 
+                // todo #5 執行此 App 去按黃色按鈕 看會如何?
+                #endregion
+                #region   #6 
+                // todo #6
+                //  將下行 select  top 3 * from products 改成 select  top 3 * from product
+                //  再執行此 App 去按黃色按鈕 看會如何?
+                #endregion
+                sqlCmd.CommandText = "select  top 3 * from products";
+                System.Data.SqlClient.SqlDataReader sdr =  sqlCmd.ExecuteReader();
+                #region  #3 以上面產生的 SqlDataReader 類別的物件
+                //    todo #3 來呼叫執行個體方法 Read 此方法沒設計引數(或稱呼為參數)
+                //    並以呼叫 此方法 當作 while 迴圈的條件
+                #endregion
+                sdr.Read();
+                #region   #4  
+                // todo #4  觀察 while 迴圈的重複動作的 { } 區段已寫好如下五行
+                #endregion
+                {
+                    string pn = $"{sdr["ProductName"]}".PadRight(50);
+                    pn = pn + $" {sdr["unitPrice"]:c2}".PadLeft(20);
+                    listBoxInTabCreateObjects.Items.Add( pn  );
+                }// end of while
+            }
+            catch ( Exception exp)
+            {
+                MessageBox.Show("msit exception: " + exp.Message);
+            }
+            finally
+            {
+                #region   #7 
+                // todo #7
+                //  觀察下行 conn != null && conn.State == ConnectionState.Open
+                #endregion
+                if (conn != null && conn.State == ConnectionState.Open)
+                {
+                    try
+                    {
+                        conn.Close();
+                    }
+                    catch (Exception deepExp)
+                    {
+
+                        MessageBox.Show(" conn.Close() exception: " + deepExp.Message);
+                    }
+                }// end of if
+            }// end of finally
+
+
+        }
+    }
+}
