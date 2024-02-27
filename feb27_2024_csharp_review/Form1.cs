6fcb3cfdcc20248a0296ff2225b73806891d5fca
HWShawn <ondlraehsconegrb@gmail.com>
2024-02-27 18:03:15 +0800

    022724_update(2)

--- feb27_2024_csharp_review/Form1.cs
+++ feb27_2024_csharp_review/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,7 +31,7 @@ namespace feb27_2024_csharp_review
                 // todo #0 產生 System.Data.SqlClient 命名空間下的 SqlConnection 類別的物件
                 //  並將此 產生的物件的參考存到上行宣告的 conn 變數
                 #endregion
-
+                conn = new SqlConnection();
 
 
                 conn.ConnectionString = "server=.;initial catalog=northwind;integrated security=true";
@@ -38,14 +39,14 @@ namespace feb27_2024_csharp_review
                 // todo #1 以上面產生的 SqlConnection 類別的物件
                 //    來呼叫執行個體方法 Open 此方法沒設計引數(或稱呼為參數)
                 #endregion
-                
-                System.Data.SqlClient.SqlCommand sqlCmd = null;
+                conn.Open();
+                SqlCommand sqlCmd = null;
                 #region  #2
                 // todo #2 以上面產生的 SqlConnection 類別的物件
                 //    來呼叫執行個體方法 CreateCommand 此方法沒設計引數(或稱呼為參數)
                 //    並將呼叫 此方法的傳回值 存到上行宣告的 sqlCmd 變數
                 #endregion
-                conn.Open();
+                sqlCmd = conn.CreateCommand();
 
                 #region   #5 
                 // todo #5 執行此 App 去按黃色按鈕 看會如何?
@@ -56,12 +57,12 @@ namespace feb27_2024_csharp_review
                 //  再執行此 App 去按黃色按鈕 看會如何?
                 #endregion
                 sqlCmd.CommandText = "select  top 3 * from products";
-                System.Data.SqlClient.SqlDataReader sdr =  sqlCmd.ExecuteReader();
+                SqlDataReader sdr =  sqlCmd.ExecuteReader();
                 #region  #3 以上面產生的 SqlDataReader 類別的物件
                 //    todo #3 來呼叫執行個體方法 Read 此方法沒設計引數(或稱呼為參數)
                 //    並以呼叫 此方法 當作 while 迴圈的條件
                 #endregion
-                sdr.Read();
+                while (sdr.Read()) 
                 #region   #4  
                 // todo #4  觀察 while 迴圈的重複動作的 { } 區段已寫好如下五行
                 #endregion
